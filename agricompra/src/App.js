f5a143cc0f46ac5bd08a3bcdac0756277b8c4725
danigb14 <danigar.gb@icloud.com>
2024-12-03 00:53:47 -0600

    Agregue apartado nosotros
    le di estilo al apartado, y le puse tambien un poco de texto

--- agricompra/src/App.js
+++ agricompra/src/App.js
@@ -6,6 +6,7 @@ import Productos from './Productos';
 import ProductosPage from './ProductosPage';
 import Account from './Account';
 import Contacto from './Contacto';
+import Nosotros from './Nosotros';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Carousel />
           <Productos />
           <Contacto />
+          <Nosotros />
         </>} />
         <Route path="/productos" element={<Productos />} /> {/* Asegúrate de que la ruta esté en minúsculas */}
         <Route path="/ProductosPage" element={<ProductosPage />} />
---
+++ agricompra/src/Nosotros.js
@@ -0,0 +1,19 @@
+import './Nosotros.css';
+
+const Nosotros = () => {
+    return (
+        <div>
+            
+            <div id="descripcion" className="descripcion">
+                <h2 className="descripcion-titulo">¿A qué nos dedicamos?</h2>
+                <p className="descripcion-texto">
+                    En Agricompra, nos dedicamos a ofrecer los mejores productos agrícolas directamente del productor al consumidor. 
+                    Nuestro objetivo es facilitar el acceso a productos frescos y de alta calidad, apoyando a los agricultores locales 
+                    y promoviendo prácticas sostenibles.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Nosotros;
