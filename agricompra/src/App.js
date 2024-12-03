8161e948242163bfb90fd14eac9e4e4d91156824
ItielSanz <itiel188@gmail.com>
2024-12-03 01:13:33 -0600

    xD

--- agricompra/src/App.js
+++ agricompra/src/App.js
@@ -6,7 +6,6 @@ import Productos from './Productos';
 import ProductosPage from './ProductosPage';
 import Account from './Account';
 import Contacto from './Contacto';
-import Nosotros from './Nosotros';
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Carousel />
           <Productos />
           <Contacto />
-          <Nosotros />
         </>} />
         <Route path="/productos" element={<Productos />} /> {/* Asegúrate de que la ruta esté en minúsculas */}
         <Route path="/ProductosPage" element={<ProductosPage />} />
