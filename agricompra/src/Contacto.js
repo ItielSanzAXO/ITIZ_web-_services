1d3aab9abed9828e1911b5da1f5e25bf68215028
ItielSanz <itiel188@gmail.com>
2024-12-03 01:12:53 -0600

    Rompi el contacto

--- agricompra/src/Contacto.js
+++ agricompra/src/Contacto.js
@@ -15,21 +15,20 @@ const Contacto = () => {
                         <strong>Correo de la empresa:</strong> 
                         <a href="mailto:agricompra@contacto.com" className="contacto-link"> agricompra@contacto.com</a>
                     </p>
+                    <div className="map-container">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6439.527370430567!2d-99.05549249005908!3d19.380319842274567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fd0614558e4b%3A0x3760847a107a0b9f!2sTecNM%20%7C%20Tecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20Campus%20Iztapalapa!5e1!3m2!1ses-419!2sus!4v1733202327813!5m2!1ses-419!2sus"
+                            width="300"
+                            height="225"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
                 </div>
                 <div className="logo-container">
                     <img src="/img/logo_letras.jpeg" alt="Logo" className="logo-image" />
-                    
-                </div>
-                <div className="map-container">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6439.527370430567!2d-99.05549249005908!3d19.380319842274567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fd0614558e4b%3A0x3760847a107a0b9f!2sTecNM%20%7C%20Tecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20Campus%20Iztapalapa!5e1!3m2!1ses-419!2sus!4v1733202327813!5m2!1ses-419!2sus"
-                        width="300"
-                        height="225"
-                        style={{ border: 0 }}
-                        allowFullScreen=""
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
                 </div>
             </div>
         </div>
--- agricompra/src/Nosotros.js
+++ agricompra/src/Nosotros.js
@@ -1,17 +1,13 @@
-import './Nosotros.css';
+import React from 'react';
+import './Nosotros.css'; // Usa el archivo CSS de Nosotros
 
 const Nosotros = () => {
     return (
-        <div>
-            
-            <div id="descripcion" className="descripcion">
-                <h2 className="descripcion-titulo">¿A qué nos dedicamos?</h2>
-                <p className="descripcion-texto">
-                    En Agricompra, nos dedicamos a ofrecer los mejores productos agrícolas directamente del productor al consumidor. 
-                    Nuestro objetivo es facilitar el acceso a productos frescos y de alta calidad, apoyando a los agricultores locales 
-                    y promoviendo prácticas sostenibles.
-                </p>
-            </div>
+        <div className="descripcion">
+            <h2 className="descripcion-titulo">Nosotros</h2>
+            <p className="descripcion-texto">
+                Somos una empresa dedicada a la venta de productos agrícolas de alta calidad. Nuestro objetivo es proporcionar a nuestros clientes los mejores productos y servicios.
+            </p>
         </div>
     );
 };
